fd6552aa73c1fa2535d684e4206a87cf872336f7
aladrak <sashakhong@gmail.com>
2025-02-23 22:05:46 +0700

    upd lab2

--- lab2/main.cpp
+++ lab2/main.cpp
@@ -1,6 +1,42 @@
+// Вариант 9
 #include <iostream>
+#include <ctime>
+#include <fstream>
+#include <string>
 using namespace std;
 
-int main() {
+int dwRand() { return (rand() % 7 + 1) * 100 + (rand() % 8) * 10 + (rand() % 8); }
+
+int* readFile(char *path) 
+{
+    ifstream f; int *arr {new int[]}; 
+    f.open(path, ios::in);
+    if (f.is_open()) {
+        while (f.getline()) {}
+    }
+    f.close();
+}
+
+void GenerateDataset (char *filename, int num) 
+{
+    ofstream f;
+    f.open(filename, ios::out);
+    for (int i = 0; i < num; i++) {
+        f << dwRand() << endl;
+    }
+    f.close();
+}
+
+int SortDataset (char *filename) // Сортировка Шелла
+{
+    return 0;
+}
+
+
+int main() 
+{
+    srand(time(NULL));
+    char *f = new char*[] = "./data.txt";
+    GenerateDataset(f, 40);
     cout << "Hello" << endl;
 }
