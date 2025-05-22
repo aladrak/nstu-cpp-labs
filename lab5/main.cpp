b6aa47e5185db744c5c17147576aa71e63977eb5
aladrak <sashakhong@gmail.com>
2025-05-22 14:53:41 +0700

    complete

--- lab5/main.cpp
+++ lab5/main.cpp
@@ -14,18 +14,17 @@
 using namespace std;
 
 struct EDGE {
-    int y;          // Вершина, с которой связана текущая вершина
-    int weight;     // Вес ребра (степень дружбы)
-    EDGE *pnext;    // Указатель на следующее ребро
+    int y;
+    EDGE *pnext;
 };
 
 struct GRAPH {
     bool directed;
-    int nvertices;  // Количество вершин фактическое
-    int nedges;     // Количество ребер фактическое
-    string *vertices;// Массив названий вершин
-    int degrees[MAXV];
-    EDGE *edges[MAXV];
+    int nvertices;      // Количество вершин
+    int nedges;         // Количество рёбер
+    string *vertices;   // Названия вершин
+    int degrees[MAXV];  // Степени вершин
+    EDGE *edges[MAXV];  // Список смежности
 };
 
 void initGraph(GRAPH *g, bool directed) {
@@ -33,24 +32,21 @@ void initGraph(GRAPH *g, bool directed) {
     g->nedges = 0;
     g->directed = directed;
 
-    EDGE **edgePtr = g->edges;
-    for (int i = 0; i < MAXV; ++i, ++edgePtr) *edgePtr = nullptr;
-    
-    int *degreePtr = g->degrees;
-    for (int i = 0; i < MAXV; ++i, ++degreePtr) *degreePtr = 0;
+    for (int i = 0; i < MAXV; ++i) {
+        g->edges[i] = nullptr;
+        g->degrees[i] = 0;
+    }
 }
 
-// Добавление ребра в граф
 void insertEdge(GRAPH *g, int x, int y, bool directed) {
     EDGE *p = new EDGE();
     p->y = y;
-    p->weight = 1; // Степень дружбы (вес ребра)
     p->pnext = g->edges[x];
     g->edges[x] = p;
     g->degrees[x]++;
 
     if (!directed) {
-        insertEdge(g, y, x, true); // Добавляем обратное ребро для неориентированного графа
+        insertEdge(g, y, x, true);
     } else {
         g->nedges++;
     }
@@ -66,55 +62,64 @@ void reverse(int *begin, int *end) {
     }
 }
 
-// Чтение графа из файлов
-void readGraph(GRAPH *g, bool directed, char *efilename, char *vfilename) {
+void readGraph(GRAPH *g, bool directed, const char *efilename, const char *vfilename) {
     initGraph(g, directed);
 
-    ifstream f(vfilename, ios::in);
-    if (!f.is_open()) { cerr << "File read error!" << endl; return; }
-
-    for (string s; getline(f, s); ++(g->nvertices));
-    f.clear();
-    f.seekg(0, f.beg);
-    g->vertices = new string[g->nvertices];
-    string *vertexPtr = g->vertices;
-    for (string s; getline(f, s); ++vertexPtr)
-        *vertexPtr = s;
-    f.close();
-
-    f.open(efilename, ios::in);
-    if (!f.is_open()) { cerr << "File read error!" << endl; return; }
-
-    for (string s; getline(f, s);) {
-        int space = s.find(" ");
-        insertEdge(
-            g,
-            stoi(s.substr(0, space)),                   // x
-            stoi(s.substr(space + 1, s.length() - 1)),  // y
-            g->directed
-        );
+    ifstream vf(vfilename);
+    if (!vf.is_open()) {
+        cerr << "Read error!" << endl;
+        return;
     }
-    f.close();
+
+    for (string s; getline(vf, s); ++g->nvertices);
+    vf.clear();
+    vf.seekg(0);
+    g->vertices = new string[g->nvertices];
+
+    for (int i = 0; i < g->nvertices; ++i) {
+        getline(vf, g->vertices[i]);
+    }
+    vf.close();
+
+    ifstream ef(efilename);
+    if (!ef.is_open()) {
+        cerr << "Read error!" << endl;
+        return;
+    }
+
+    string s;
+    while (getline(ef, s)) {
+        int space = s.find(" ");
+        int x = stoi(s.substr(0, space));
+        int y = stoi(s.substr(space + 1));
+        insertEdge(g, x, y, g->directed);
+    }
+    ef.close();
 }
 
 void printGraph(GRAPH *g) {
-    EDGE **edgePtr = g->edges;
-    string *vertexPtr = g->vertices;
-    int *degreePtr = g->degrees;
-
-    for (int i = 0; i < g->nvertices; ++i, ++edgePtr, ++vertexPtr, ++degreePtr) {
-        EDGE *cur = *edgePtr;
-        cout << i << " " << *vertexPtr << " - (" << *degreePtr << "): ";
-        while (cur) 
-        {
-            cout << " " << cur->y << " " << g->vertices[cur->y];
-            cur = cur->pnext;
+    cout << "Graph:" << endl;
+    for (int i = 0; i < g->nvertices; ++i) {
+        cout << i << ": " << g->vertices[i] << " -> ";
+        EDGE *curr = g->edges[i];
+        while (curr) {
+            cout << curr->y << " ";
+            curr = curr->pnext;
         }
         cout << endl;
     }
 }
 
-// Поиск кратчайшего пути между двумя вершинами (поиск в ширину)
+bool hasSelfLoop(GRAPH *g, int vertex) {
+    EDGE *e = g->edges[vertex];
+    while (e != nullptr) {
+        if (e->y == vertex)
+            return true;
+        e = e->pnext;
+    }
+    return false;
+}
+
 bool findShortestPath(GRAPH *g, int start, int end, int *path, int &pathLength) {
     if (start == end) {
         path[0] = start;
@@ -122,29 +127,28 @@ bool findShortestPath(GRAPH *g, int start, int end, int *path, int &pathLength) 
         return true;
     }
 
-    queue<int> q;
     bool visited[MAXV] = {false};
     int parent[MAXV] = {-1};
+    queue<int> q;
 
     q.push(start);
     visited[start] = true;
 
     while (!q.empty()) {
-        int current = q.front();
-        q.pop();
+        int current = q.front(); q.pop();
 
-        EDGE *cur = g->edges[current];
-        while (cur) {
-            int neighbor = cur->y;
+        EDGE *e = g->edges[current];
+        while (e != nullptr) {
+            int neighbor = e->y;
+
             if (!visited[neighbor]) {
                 visited[neighbor] = true;
                 parent[neighbor] = current;
                 q.push(neighbor);
 
                 if (neighbor == end) {
-                    // Восстанавливаем путь
-                    int node = end;
                     pathLength = 0;
+                    int node = end;
                     while (node != start) {
                         path[pathLength++] = node;
                         node = parent[node];
@@ -154,45 +158,56 @@ bool findShortestPath(GRAPH *g, int start, int end, int *path, int &pathLength) 
                     return true;
                 }
             }
-            cur = cur->pnext;
+            e = e->pnext;
         }
     }
 
-    return false; // Путь не найден
+    return false;
 }
 
 int main() {
     GRAPH *g = new GRAPH;
     initGraph(g, false);
 
-    char path_edges[] = "./edges.txt";
-    char path_vertices[] = "./vertices.txt";
+    const char *efilename = "./edges.txt";
+    const char *vfilename = "./vertices.txt";
 
-    readGraph(g, g->directed, path_edges, path_vertices);
+    readGraph(g, false, efilename, vfilename);
     printGraph(g);
+
     int userVertex;
     cout << "\nEnter your vertex number: ";
     cin >> userVertex;
 
     int path[MAXV];
-    int pathLength;
-    if (findShortestPath(g, userVertex, userVertex, path, pathLength)) {
-        cout << "You are a friend to yourself!" << endl;
-    } else {
-        cout << "You are not a friend to yourself." << endl;
+    int pathLength = 0;
+
+    if (hasSelfLoop(g, userVertex)) {
+        cout << "Yes, you are a friend to yourself (direct self-loop)." << endl;
+    }
+    else if (findShortestPath(g, userVertex, userVertex, path, pathLength) && pathLength > 1) {
+        cout << "Yes, you are a friend to yourself through others:" << endl;
+        for (int i = 0; i < pathLength; ++i) {
+            cout << g->vertices[path[i]];
+            if (i < pathLength - 1) cout << " -> ";
+        }
+        cout << endl;
+    }
+    else {
+        cout << "No, you are not a friend to yourself." << endl;
+        cout << "There is no loop or connection to yourself." << endl;
     }
 
-    // Поиск кратчайшего пути между двумя вершинами
     int start, end;
-    cout << "\nEnter start vertex: ";
+    cout << "\nEnter the starting vertex: ";
     cin >> start;
-    cout << "Enter end vertex: ";
+    cout << "Enter the ending vertex: ";
     cin >> end;
 
     if (findShortestPath(g, start, end, path, pathLength)) {
-        cout << "Shortest path from " << start << " to " << end << ": ";
-        for (int i = 0; i < pathLength; i++) {
-            cout << path[i];
+        cout << "The shortest path from " << start << " to " << end << ": ";
+        for (int i = 0; i < pathLength; ++i) {
+            cout << g->vertices[path[i]];
             if (i < pathLength - 1) cout << " -> ";
         }
         cout << endl;
@@ -200,5 +215,5 @@ int main() {
         cout << "No path found between " << start << " and " << end << "." << endl;
     }
 
-    return 0;
+    delete g;
 }
